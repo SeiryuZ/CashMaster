f6f086e70d9a7ba1bf63ab7a7aef5a22e10aa8be
adamhw <adamhw@users.noreply.github.com>
2018-04-29 10:00:17 +0700

    Able to move between activities

--- app/src/main/java/com/halim/adam/cashmaster/TransactionHistory.java
+++ app/src/main/java/com/halim/adam/cashmaster/TransactionHistory.java
@@ -1,7 +1,9 @@
 package com.halim.adam.cashmaster;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class TransactionHistory extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class TransactionHistory extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transaction_history);
+    }
+
+    public void MoveToNewSpendingActivity(View view){
+        Intent intent = new Intent(this, AddNewSpending.class);
+        startActivity(intent);
+
     }
 }
