2c31e0d27d1f08b164ec80b335fabfc049e36ebc
adamhw <adamhw@users.noreply.github.com>
2018-05-01 14:17:15 +0700

    Add more textfields and buttons

--- app/src/main/java/com/halim/adam/cashmaster/Objects/Category.java
+++ app/src/main/java/com/halim/adam/cashmaster/Objects/Category.java
@@ -5,4 +5,5 @@ package com.halim.adam.cashmaster.Objects;
  */
 
 public class Category {
+    private int id;
 }
--- app/src/main/java/com/halim/adam/cashmaster/Objects/Spending.java
+++ app/src/main/java/com/halim/adam/cashmaster/Objects/Spending.java
@@ -5,4 +5,6 @@ package com.halim.adam.cashmaster.Objects;
  */
 
 public class Spending {
+    private int id;
+    private String name;
 }
