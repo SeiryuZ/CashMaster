2b35bff5ef3b3f49f68878696eb7276b6c337698
adamhw <adamhw@users.noreply.github.com>
2018-05-10 13:04:37 +0700

    working db read and write

--- app/src/main/java/com/halim/adam/cashmaster/DatabaseHelper.java
+++ app/src/main/java/com/halim/adam/cashmaster/DatabaseHelper.java
@@ -1,8 +1,35 @@
 package com.halim.adam.cashmaster;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Debug;
+import android.util.Log;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
-    private static final String DATABASE_NAME = "CashMaster.db";
-    private static final int VERSION = 1;
+    private static final String DATABASE_NAME = "cash_master.db";
+    private static final int DATABASE_VERSION = 3;
+
+    public DatabaseHelper(Context context){
+        super(context, DATABASE_NAME,null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sql = "CREATE TABLE category ( id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT UNIQUE );";
+        db.execSQL(sql);
+        sql = "CREATE TABLE income ( id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, price NUMERIC, date TEXT DEFAULT 'date(''now'')');";
+        db.execSQL(sql);
+        sql = "CREATE TABLE jar ( id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, portion INTEGER );";
+        db.execSQL(sql);
+        sql = "CREATE TABLE spending ( id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, date TEXT DEFAULT 'date(''now'')', price NUMERIC, categoryId INTEGER, jarId INTEGER, FOREIGN KEY(categoryId) REFERENCES category(id) ON DELETE SET NULL );";
+        db.execSQL(sql);
+        sql = "INSERT INTO category (name) VALUES ('Drinks');";
+        db.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
 }
--- app/src/main/java/com/halim/adam/cashmaster/TransactionHistory.java
+++ app/src/main/java/com/halim/adam/cashmaster/TransactionHistory.java
@@ -1,16 +1,33 @@
 package com.halim.adam.cashmaster;
 
+import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
-public class TransactionHistory extends AppCompatActivity {
+public class TransactionHistory extends Activity {
+    protected Cursor cursor;
+    TextView transactionList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transaction_history);
+        transactionList = (TextView) findViewById(R.id.transactionList);
+
+        SQLiteDatabase db = new DatabaseHelper(this).getWritableDatabase();
+        cursor = db.rawQuery("SELECT * FROM category", null);
+        cursor.moveToFirst();
+        if(cursor.getCount()<=0){
+            transactionList.setText("Still Empty!");
+        }
+        else{
+            transactionList.setText(cursor.getString((cursor.getColumnIndex("name"))));
+        }
     }
 
     public void MoveToNewSpendingActivity(View view){
