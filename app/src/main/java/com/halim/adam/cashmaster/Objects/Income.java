5ce6af872fdd70f1bedb6d73d4f9792f11449545
adamhw <adamhw@users.noreply.github.com>
2018-05-11 07:53:16 +0700

    getter and setter for classes

--- app/src/main/java/com/halim/adam/cashmaster/Objects/Income.java
+++ app/src/main/java/com/halim/adam/cashmaster/Objects/Income.java
@@ -11,4 +11,36 @@ public class Income {
     private String name;
     private float price;
     private Date date;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
 }
--- app/src/main/java/com/halim/adam/cashmaster/Objects/Spending.java
+++ app/src/main/java/com/halim/adam/cashmaster/Objects/Spending.java
@@ -13,4 +13,52 @@ public class Spending {
     private float price;
     private int categoryId;
     private int jarId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public int getJarId() {
+        return jarId;
+    }
+
+    public void setJarId(int jarId) {
+        this.jarId = jarId;
+    }
 }
