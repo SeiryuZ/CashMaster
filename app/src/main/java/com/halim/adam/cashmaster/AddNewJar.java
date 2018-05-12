c507c71ada45b06300cd81e49f5e9a0ce4727053
adamhw <adamhw@users.noreply.github.com>
2018-05-12 10:37:46 +0700

    implement compileStatement

---
+++ app/src/main/java/com/halim/adam/cashmaster/AddNewJar.java
@@ -0,0 +1,13 @@
+package com.halim.adam.cashmaster;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AddNewJar extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_jar);
+    }
+}
--- app/src/main/java/com/halim/adam/cashmaster/DatabaseHelper.java
+++ app/src/main/java/com/halim/adam/cashmaster/DatabaseHelper.java
@@ -3,6 +3,7 @@ package com.halim.adam.cashmaster;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteStatement;
 import android.os.Debug;
 import android.util.Log;
 
@@ -19,13 +20,17 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         String sql = "CREATE TABLE category ( id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT UNIQUE );";
-        db.execSQL(sql);
+        SQLiteStatement stmt = db.compileStatement(sql);
+        stmt.execute();
         sql = "CREATE TABLE income ( id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, price NUMERIC, date TEXT DEFAULT (date('now')));";
-        db.execSQL(sql);
+        stmt = db.compileStatement(sql);
+        stmt.execute();
         sql = "CREATE TABLE jar ( id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, portion INTEGER );";
-        db.execSQL(sql);
+        stmt = db.compileStatement(sql);
+        stmt.execute();
         sql = "CREATE TABLE spending ( id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, date TEXT DEFAULT (date('now')), price NUMERIC, categoryId INTEGER, jarId INTEGER, FOREIGN KEY(categoryId) REFERENCES category(id) ON DELETE SET NULL );";
-        db.execSQL(sql);
+        stmt = db.compileStatement(sql);
+        stmt.execute();
     }
 
     @Override
@@ -44,41 +49,48 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public void InsertCategory(String name){
         SQLiteDatabase db = this.getWritableDatabase();
         String sql = "INSERT INTO category (name) VALUES ('" + name + "');";
-        db.execSQL(sql);
+        SQLiteStatement stmt = db.compileStatement(sql);
+        stmt.executeInsert();
         db.close();
     }
 
     public void InsertIncome(String name, float price, Date date){
         SQLiteDatabase db = this.getWritableDatabase();
         String sql = "INSERT INTO income (name, price, date) VALUES ('" + name + "', '" + price + "', '" + date +  "');";
+        SQLiteStatement stmt = db.compileStatement(sql);
+        stmt.executeInsert();
         db.close();
     }
 
     public void InsertIncome(String name, float price){
         SQLiteDatabase db = this.getWritableDatabase();
         String sql = "INSERT INTO income (name, price) VALUES ('" + name + "', '" + price + "');";
-        db.execSQL(sql);
+        SQLiteStatement stmt = db.compileStatement(sql);
+        stmt.executeInsert();
         db.close();
     }
 
     public void InsertJar(String name, int portion){
         SQLiteDatabase db = this.getWritableDatabase();
         String sql = "INSERT INTO jar (name, portion) VALUES ('" + name + "', '" + portion + "');";
-        db.execSQL(sql);
+        SQLiteStatement stmt = db.compileStatement(sql);
+        stmt.executeInsert();
         db.close();
     }
 
     public void InsertSpending(String name, float price, int categoryId, int jarId, Date date){
         SQLiteDatabase db = this.getWritableDatabase();
         String sql = "INSERT INTO spending (name, date, price, categoryId, jarId) VALUES ('" + name + "', '" + date + "', '" + price + "', '" + categoryId + "', '" + jarId +  "');";
-        db.execSQL(sql);
+        SQLiteStatement stmt = db.compileStatement(sql);
+        stmt.executeInsert();
         db.close();
     }
 
     public void InsertSpending(String name, float price, int categoryId, int jarId){
         SQLiteDatabase db = this.getWritableDatabase();
         String sql = "INSERT INTO spending (name, price, categoryId, jarId) VALUES ('" + name + "', '" + price + "', '" + categoryId + "', '" + jarId +  "');";
-        db.execSQL(sql);
+        SQLiteStatement stmt = db.compileStatement(sql);
+        stmt.executeInsert();
         db.close();
     }
 
